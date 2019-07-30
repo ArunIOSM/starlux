281884e3b5f9034f0e415ea86d256164ac2f132a
Arun Kumar <arun.kumar12@arun-kumar12.local>
2019-07-30 16:41:10 +0530

    UITest

--- StarLuxUITests/StarLuxUITests.swift
+++ StarLuxUITests/StarLuxUITests.swift
@@ -4,6 +4,7 @@ import XCTest
 
 class StarLuxUITests: XCTestCase {
 
+    
     override func setUp() {
         
         // Put setup code here. This method is called before the invocation of each test method in the class.
